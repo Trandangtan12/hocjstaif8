14ea34cd64cc13dd94a5e885f3f538ebdae51284
Trandangtan12 <80817212+Trandangtan12@users.noreply.github.com>
2021-10-18 00:14:05 +0700

    update code Mucsic player

--- Cac_Bai_tap_Thuc_Hanh/MusicPlayer/index.js
+++ Cac_Bai_tap_Thuc_Hanh/MusicPlayer/index.js
@@ -14,81 +14,85 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-const player = $('.player');
-const cd = $('.cd');
-const heading = $('header h2');
-const cdThumb = $('.cd-thumb');
-const audio = $('#audio');
-const playBtn = $('.btn-toggle-play')
+const player = $(".player");
+const cd = $(".cd");
+const heading = $("header h2");
+const cdThumb = $(".cd-thumb");
+const audio = $("#audio");
+const playBtn = $(".btn-toggle-play");
+const progress = $("#progress");
+const nextBtn = $(".btn-next");
+const prevBtn = $(".btn-prev");
 
 const app = {
-    currentIndex: 0,
-    isPlaying: false,
-    songs: [
-        {
-            name: 'Song 1',
-            singer: 'singer 1',
-            path: './music/song (1).mp3',
-            image: './img/song (1).jpg'
-        },
-        {
-            name: 'Song 2',
-            singer: 'singer 2',
-            path: './music/song (2).mp3',
-            image: './img/song (2).jpg'
-        },
-        {
-            name: 'Song 3',
-            singer: 'singer 3',
-            path: './music/song (3).mp3',
-            image: './img/song (3).jpg'
-        },
-        {
-            name: 'Song 4',
-            singer: 'singer 4',
-            path: './music/song (4).mp3',
-            image: './img/song (4).jpg'
-        },
-        {
-            name: 'Song 5',
-            singer: 'singer 5',
-            path: './music/song (5).mp3',
-            image: './img/song (5).jpg'
-        },
-        {
-            name: 'Song 6',
-            singer: 'singer 6',
-            path: './music/song (6).mp3',
-            image: './img/song (6).jpg'
-        },
-        {
-            name: 'Song 7',
-            singer: 'singer 7',
-            path: './music/song (7).mp3',
-            image: './img/song (7).jpg'
-        },
-        {
-            name: 'Song 8',
-            singer: 'singer 8',
-            path: './music/song (8).mp3',
-            image: './img/song (8).jpg'
-        },
-        {
-            name: 'Song 9',
-            singer: 'singer 9',
-            path: './music/song (9).mp3',
-            image: './img/song (9).jpg'
-        },
-        {
-            name: 'Song 10',
-            singer: 'singer 10',
-            path: './music/song (10).mp3',
-            image: './img/song (10).jpg'
-        }
-    ],
-    render: function () {
-        const htmls = this.songs.map((song) => {
-            return `
+  currentIndex: 0,
+  isPlaying: false,
+  songs: [
+    {
+      name: "Song 1",
+      singer: "singer 1",
+      path: "./music/song (1).mp3",
+      image: "./img/song (1).jpg",
+    },
+    {
+      name: "Song 2",
+      singer: "singer 2",
+      path: "./music/song (2).mp3",
+      image: "./img/song (2).jpg",
+    },
+    {
+      name: "Song 3",
+      singer: "singer 3",
+      path: "./music/song (3).mp3",
+      image: "./img/song (3).jpg",
+    },
+    {
+      name: "Song 4",
+      singer: "singer 4",
+      path: "./music/song (4).mp3",
+      image: "./img/song (4).jpg",
+    },
+    {
+      name: "Song 5",
+      singer: "singer 5",
+      path: "./music/song (5).mp3",
+      image: "./img/song (5).jpg",
+    },
+    {
+      name: "Song 6",
+      singer: "singer 6",
+      path: "./music/song (6).mp3",
+      image: "./img/song (6).jpg",
+    },
+    {
+      name: "Song 7",
+      singer: "singer 7",
+      path: "./music/song (7).mp3",
+      image: "./img/song (7).jpg",
+    },
+    {
+      name: "Song 8",
+      singer: "singer 8",
+      path: "./music/song (8).mp3",
+      image: "./img/song (8).jpg",
+    },
+    {
+      name: "Song 9",
+      singer: "singer 9",
+      path: "./music/song (9).mp3",
+      image: "./img/song (9).jpg",
+    },
+    {
+      name: "Song 10",
+      singer: "singer 10",
+      path: "./music/song (10).mp3",
+      image: "./img/song (10).jpg",
+    },
+  ],
+  render: function () {
+    const htmls = this.songs
+      .map((song) => {
+        return `
             <div class="song">
             <div class="thumb" style="background-image: url('${song.image}')">
             </div>
@@ -99,79 +103,138 @@ const app = {
             <div class="option">
               <i class="fas fa-ellipsis-h"></i>
             </div>
-          </div>`
-        }).join("")
-        $('.playlist').innerHTML = htmls
-    },
-    defineProperties: function () {
-        Object.defineProperty(this, 'currentSong', {
-            get: function () {
-                return this.songs[this.currentIndex]
-            }
-        })
-    },
-    handleEvents: function () {
-        const cdWidth = cd.offsetWidth;
-        const _this = this
+          </div>`;
+      })
+      .join("");
+    $(".playlist").innerHTML = htmls;
+  },
+  defineProperties: function () {
+    Object.defineProperty(this, "currentSong", {
+      get: function () {
+        return this.songs[this.currentIndex];
+      },
+    });
+  },
+  handleEvents: function () {
+    const cdWidth = cd.offsetWidth;
+    const _this = this;
 
-        //Xử lý khi phóng to / thu nhỏ CD
-        document.onscroll = function () {
-            const scrollY = window.scrollY
-            const newCdWidth = cdWidth - scrollY
+    //Xử lý CD quay / dừng
+    const cdThumbAnimate = cdThumb.animate([{ transform: "rotate(360deg)" }], {
+      duration: 15000, // 10 giây
+      iterations: Infinity,
+    });
+    cdThumbAnimate.pause();
 
-            cd.style.width = newCdWidth > 0 ? newCdWidth + 'px' : 0;
-            cd.style.opacity = newCdWidth / cdWidth;
+    //Xử lý khi phóng to / thu nhỏ CD
+    document.onscroll = function () {
+      const scrollY = window.scrollY;
+      const newCdWidth = cdWidth - scrollY;
+
+      cd.style.width = newCdWidth > 0 ? newCdWidth + "px" : 0;
+      cd.style.opacity = newCdWidth / cdWidth;
+    };
+
+    // Xử lý khi click player
+    playBtn.onclick = function () {
+      // Code tư duy thôi chức như này là xấu
+      // if(_this.isPlaying) {
+      //     _this.isPlaying = false;
+      //     audio.pause()
+      //     player.classList.remove('playing')
+      // }else {
+      //     _this.isPlaying = true
+      //     audio.play()
+      //     player.classList.add('playing')
+      // }
+      if (_this.isPlaying) {
+        audio.pause();
+      } else {
+        audio.play();
+      }
+
+      // Khi song được play
+      audio.onplay = function () {
+        _this.isPlaying = true;
+        player.classList.add("playing");
+        cdThumbAnimate.play();
+      };
+
+      //khi song bị pause
+      audio.onpause = function () {
+        _this.isPlaying = false;
+        player.classList.remove("playing");
+        cdThumbAnimate.pause();
+      };
+
+      //khi tiến độ bài hát thay đổi
+      audio.ontimeupdate = function () {
+        if (audio.duration) {
+          const progressPercent = Math.floor(
+            (audio.currentTime / audio.duration) * 100
+          );
+          progress.value = progressPercent;
         }
+      };
+    };
 
-        // Xử lý khi click player
-        playBtn.onclick = function () {
-            // Code tư duy thôi chức như này là xấu
-            // if(_this.isPlaying) {
-            //     _this.isPlaying = false;
-            //     audio.pause()
-            //     player.classList.remove('playing')
-            // }else {
-            //     _this.isPlaying = true
-            //     audio.play()
-            //     player.classList.add('playing')
-            // }
+    //Xử lý khi tua song
+    progress.oninput = function (e) {
+      audio.pause();
+      setTimeout(() => {
+        audio.play();
+      }, 500);
+      const seekTime = (audio.duration / 100) * e.target.value;
+      audio.currentTime = seekTime;
+    };
+    //Khi next song
+    nextBtn.onclick = function () {
+      _this.nextSong();
+      audio.play();
+      player.classList.add("playing");
+      cdThumbAnimate.play();
+    };
 
-            audio.onplay =function () {
-                _this.isPlaying = true
-                player.classList.add('playing')
-            }
-            audio.onpause =function () {
-                _this.isPlaying = false
-                player.classList.remove('playing')
-            }
-
-            if(_this.isPlaying) {
-                audio.pause()
-            }else{
-                audio.play()
-            }
-
-        }
-
-    },
-    loadCurrentSong: function () {
-        heading.textContent = this.currentSong.name;
-        cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
-        audio.src = this.currentSong.path;
-    },
-
-    start: function () {
-        //Định nghĩa các thuộc tính cho object
-        this.defineProperties()
-
-        // Lắng nghe / xử lý các sự kiện (DOM event)
-        this.handleEvents()
-
-        // Tải thông tin bài hát đầu tiên vào UI khi chạy ứng dụng
-        this.loadCurrentSong()
-
-        //Render playlist
-        this.render()
+    //khi prev song
+  prevBtn.onclick = function () {
+    _this.prevSong();
+    audio.play();
+    player.classList.add("playing");
+    cdThumbAnimate.play();
+  };
+},
+  loadCurrentSong: function () {
+    heading.textContent = this.currentSong.name;
+    cdThumb.style.backgroundImage = `url('${this.currentSong.image}')`;
+    audio.src = this.currentSong.path;
+  },
+  nextSong: function () {
+    this.currentIndex++;
+    if (this.currentIndex >= this.songs.length) {
+      this.currentIndex = 0;
     }
-}
-app.start()
+    this.loadCurrentSong()
+  },
+  prevSong: function () {
+    this.currentIndex--;
+    if (this.currentIndex < 0) {
+      this.currentIndex = this.songs.length;
+    }
+    this.loadCurrentSong()
+  },
+
+  start: function () {
+    //Định nghĩa các thuộc tính cho object
+    this.defineProperties();
+
+    // Lắng nghe / xử lý các sự kiện (DOM event)
+    this.handleEvents();
+
+    // Tải thông tin bài hát đầu tiên vào UI khi chạy ứng dụng
+    this.loadCurrentSong();
+
+    //Render playlist
+    this.render();
+  },
+};
+app.start();
